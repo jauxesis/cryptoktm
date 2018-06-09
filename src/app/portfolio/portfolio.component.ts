f363f087e95bb2179f768020955d5a44fcb23249
Jitendra Patwa <jitendrapatwa135@gmail.com>
2018-06-09 22:03:39 +0530

    dsf

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,6 +14,10 @@ import { GlobalparamsService } from '../services/globalparams.service';
 })
 export class PortfolioComponent implements OnInit {
 
+  portFolio:any = [];
+  currentPrice:any = [];
+  isData:number = 0;
+
   constructor(
     private fb: FormBuilder,
     public router:Router,
@@ -25,8 +29,80 @@ export class PortfolioComponent implements OnInit {
 
   ngOnInit() {
     this.api.getAuth();
-    
+    this.getPfs();    
   }
+
+
+  getPfs(){
+    /*this.api.callPortfolioApi()
+    .then(
+      d=>{
+        console.log(d)
+        let dt = JSON.parse(JSON.stringify(d));
+        if(dt){
+          this.currentPrice = {btc_price_usd:dt.btc_price,eth_price_usd:dt.eth_price_usd};
+          let data = dt.a;
+          _.forEach(data,(value,key)=>{
+            this.portFolio.push({
+              amount:value.amount,
+              currency:(value.currency).toString().toUpperCase(),
+              currencylower:value.currency,
+              market:value.market,
+              price:value.price,
+              type:(value.type).toString().toUpperCase(),
+              typelower:value.type,
+              user:value.user
+            });  
+          });
+          this.portFolio = ;
+          this.isData = 2;
+        }else{
+          this.isData = 1;
+        }
+      },
+      e=>{
+        //console.log(e)
+        this.isData = 1;
+      }
+    ).catch(e=>{
+        //console.log(e)
+        this.isData = 1;
+    })*/
+
+
+    let dt = this.api.fakeData();
+    if(dt){
+      this.currentPrice = {btc_price_usd:dt.btc_price,eth_price_usd:dt.eth_price_usd};
+      let data = dt.a;
+      _.forEach(data,(value,key)=>{
+        let pftype = "SELL";
+        if(value.type == 'bid'){  
+          pftype = "SELL";
+        }else if(value.type == 'ask'){
+          pftype = "SELL";
+        }
+        this.portFolio.push({
+          rowid:(key+1),
+          amount_main:value.amount,
+          amount:this.api.calcsubstr(value.amount),
+          currency:(value.currency).toString().toUpperCase(),
+          currencylower:value.currency,
+          market:value.market,
+          price_main:value.price,
+          price:this.api.calcsubstr(value.price),
+          type:(value.type).toString().toUpperCase(),
+          typelower:value.type,
+          user:value.user
+        });  
+      });
+      console.log(this.portFolio,dt);
+      this.isData = 2;
+    }else{
+      this.isData = 1;
+    }
+  }
+
+
 
   
 
--- src/app/services/globalparams.service.ts
+++ src/app/services/globalparams.service.ts
@@ -9,4 +9,8 @@ export class GlobalparamsService {
   //http://13.126.248.75:8080/dataset/coin/ethereum
 
   javaLink:any = "http://192.168.4.43:7070/tranactions/0x03e2ea82c1d3ea69cfaacadeba79f42360c26939";
+
+
+  potfolioLink:any = "http://192.168.4.50:5000/get_trade_history/";
+
 }
